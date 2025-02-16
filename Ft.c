e271c64ae170ed5ef079ef04a05a3784264568c8
Guto_tassi <augustotassibmoreira@gmail.com>
2025-02-16 14:16:34 -0300

    Com listar

--- Ft.c
+++ Ft.c
@@ -11,7 +11,7 @@ typedef struct Cliente{
 
 typedef struct Camiseta{
     int id;
-    char modelo[10];
+    char modelo[10], tamanho[2];
     float preco;
 }Camiseta;
 
@@ -23,6 +23,7 @@ typedef struct Compra{
 }Compra;
 
 int tabela() {
+    //na real é o primeiro menu, tem q rever os nomes, mas nesse tem a escolha pra q dados vc quer mexer
     int escolha;
     printf("\nNo que deseja mexer?\n1 - Clientes\n2 - Camisetas\n3 - Compras\n4 - Sair\n");
     scanf("%i", &escolha);
@@ -30,6 +31,7 @@ int tabela() {
 }
 
 int menu() {
+    // aí esse é o segundo menu, q tem as ações q vc pode fazer
     int escolha;
     printf("\nBem vindo, o que deseja fazer?\n1 - Adicionar\n2 - Remover\n3 - Atualizar\n4 - Listar\n5 - Buscar\n6 - Voltar\n");
     scanf("%i", &escolha);
@@ -37,6 +39,7 @@ int menu() {
 }
 
 void identidade(int escolha, char *tipo){
+    // Esse daqui é só pra ter título mostrando em q tipo de dado vc tá mexendo
     switch(escolha){
         case 1 : strcpy(tipo, "Clientes");
             break;
@@ -47,14 +50,34 @@ void identidade(int escolha, char *tipo){
     }
 }
 
-void adicionar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int n_cl, int n_cm, int n_cp) {
+void ordenar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
+    
+}
+void adicionar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
     switch(escolha){
         case 1 : 
+            cliente[*n_cl].id=*n_cl;
             printf("Nome: ");
-            fgets();
-            n_cl++;
+            fgets(cliente[*n_cl].nome, sizeof(cliente[*n_cl].nome), stdin);
+            fgets(cliente[*n_cl].nome, sizeof(cliente[*n_cl].nome), stdin);
+            printf("Telefone: ");
+            fgets(cliente[*n_cl].telefone, sizeof(cliente[*n_cl].telefone), stdin);
+            printf("Endereco: ");
+            fgets(cliente[*n_cl].endereco, sizeof(cliente[*n_cl].endereco), stdin);
+            /*testes de funcionameto prévio:
+            printf("Nome cliente %i : %s", *n_cl, cliente[*n_cl].nome);
+            printf("Telefone cliente %i : %s", *n_cl, cliente[*n_cl].telefone);
+            printf("Endereco cliente %i : %s", *n_cl, cliente[*n_cl].endereco);*/
+            (*n_cl)++;
             break;
-        case 2 : 
+        case 2 :
+            printf("Nome: ");
+            fgets(cliente[*n_cl].nome, sizeof(cliente[*n_cl].nome), stdin);
+            fgets(cliente[*n_cl].nome, sizeof(cliente[*n_cl].nome), stdin);
+            printf("Telefone: ");
+            fgets(cliente[*n_cl].telefone, sizeof(cliente[*n_cl].telefone), stdin);
+            printf("Endereco: ");
+            fgets(cliente[*n_cl].endereco, sizeof(cliente[*n_cl].endereco), stdin); 
             n_cm++;
             break;
         case 3 : 
@@ -68,10 +91,60 @@ void adicionar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha
     
 }
 
+void remover(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
 
+}
+
+void atualizar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
+
+}
+
+void listar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
+    switch (escolha)
+    {
+        case 4:
+            if (*n_cl == 0) {
+                printf("\nNenhum cliente cadastrado.\n");
+                return;
+            }
+
+            for (int i = 0; i < *n_cl - 1; i++) {
+                for (int j = i + 1; j < *n_cl; j++) {
+                    if (strcmp(cliente[i].nome, cliente[j].nome) > 0) {
+                        Cliente temp = cliente[i];
+                        cliente[i] = cliente[j];
+                        cliente[j] = temp;
+                    }
+                }
+            }
+
+            printf("\n<===Lista de Clientes===>\n");
+            for (int i = 0; i < *n_cl; i++) {
+                cliente[i].nome[strcspn(cliente[i].nome, "\n")] = '\0';
+                
+                printf("ID: %d | Nome: %s | Telefone: %s | Endereco: %s\n", 
+                    cliente[i].id, cliente[i].nome, cliente[i].telefone, cliente[i].endereco);
+            }
+            break;
+        
+        default:
+            printf("\nOpcao invalida.\n");
+            break;
+    }
+}
+
+void buscar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
+
+}
 
 int main() {
+    //ent, tem q ver esse tanto de vari´´avel, pq eu acho q deve ficar melhor se diminuir, mas n sei
     int escolha_1, escolha_2, n_cl=0, n_cm=0, n_cp=0;
+    int *n1, *n2, *n3;
+    n1=&n_cl;
+    n2=&n_cm;
+    n3=&n_cp;
+
     char *tipo;
     tipo=malloc(10*sizeof(char));
     Cliente *cliente;
@@ -90,15 +163,15 @@ int main() {
             escolha_2 = menu();
             if(escolha_2 == 6) break;
             switch(escolha_2){
-                case 1: adicionar(cliente, camiseta, compra, escolha_1, n_cl, n_cm, n_cp);
+                case 1: adicionar(cliente, camiseta, compra, escolha_1, n1, n2, n3);
                     break;
                 /*case 2: remover(cliente, camiseta, compra);
                     break;
                 case 3: atualizar(cliente, camiseta, compra);
+                    break;*/
+                case 4: listar(cliente, camiseta, compra, escolha_2, n1, n2, n3);
                     break;
-                case 4: listar(cliente, camiseta, compra);
-                    break;
-                case 5 : buscar(cliente, camiseta, compra);
+                /*case 5 : buscar(cliente, camiseta, compra);
                     break;
                 case 6: printf("Voltando\n");
                     break;*/
@@ -106,8 +179,5 @@ int main() {
             }
         }while(escolha_2 != 6);
     }while(escolha_1 != 4);
-        
-    
-    
     return 0;
 }
