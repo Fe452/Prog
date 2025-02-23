0b4efd692cca7754236cad26f30610dca3e880e2
Guto_tassi <augustotassibmoreira@gmail.com>
2025-02-23 07:42:26 -0300

    atualizar e remover

--- Ft.c
+++ Ft.c
@@ -6,7 +6,7 @@ const int TAM = 10;
 
 typedef struct Cliente{
     int id;
-    char nome[20], telefone[16], endereco[50];
+    char nome[100], telefone[16], endereco[50];
 }Cliente;
 
 typedef struct Camiseta{
@@ -91,41 +91,44 @@ void adicionar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha
     }   
 }
 
-void remover(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
-    char clienteBusca[30];
+void remover(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp, char *buscaCliente) {
 
-    switch (escolha)
+    for (int i = 0; i < *n_cl; i++)
     {
-    case 6:
-        // se não tiver cliente
-        if (*n_cl == 0){
-            printf("\nNenhum cliente cadastrado.\n");
+        if (strcmp(cliente[i].nome, buscaCliente) == 0) {
+            printf("\nCliente ''%s'' removido\n", cliente[i].nome);
+            for (int j = i; j < *n_cl - 1; j++) {
+                cliente[j] = cliente[j + 1];  // vai deslocar os elementos pra tirar o espaço vazio
+            }
+            (*n_cl)--;
             return;
         }
-        // aqui da pra buscar o nome
-        printf("Qual cliente deseja remover?\n");
-        while (getchar() != '\n');
-        fgets(clienteBusca, sizeof(clienteBusca), stdin);
-        clienteBusca[strcspn(clienteBusca, "\n")] = '\0';
-
-        for (int i = 0; i < *n_cl; i++)
-        {
-            if (strcmp(cliente[i].nome, clienteBusca) == 0)
-            {
-                
-            }
-            
-        }
-        
-        break;
-    
-    default:
-        break;
-    }
+    } 
 }
 
-void atualizar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
-    printf("boa");
+void atualizar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp, char *buscaCliente) {
+    for (int i = 0; i < *n_cl; i++)
+    {
+        if (strcmp(cliente[i].nome, buscaCliente) == 0)
+        {
+            printf("\nAtualizando o cliente ''%s''\n", buscaCliente);
+            printf("Atualizando o nome: ");
+            fgets(cliente[i].nome, sizeof(cliente[i].nome), stdin);
+            cliente[i].nome[strcspn(cliente[i].nome, "\n")] = '\0';
+
+            printf("\nAtualizando o telefone: ");
+            fgets(cliente[i].telefone, sizeof(cliente[i].telefone), stdin);
+            cliente[i].telefone[strcspn(cliente[i].telefone, "\n")] = '\0';
+
+            printf("\nAtualizando o endereco: ");
+            fgets(cliente[i].endereco, sizeof(cliente[i].endereco), stdin);
+            cliente[i].endereco[strspn(cliente[i].endereco, "\n")] = '\0';
+
+            printf("\nCliente atualizado!");
+            return;
+        }
+    }
+    printf("Cliente não encontrado para atualização.\n");
 }
 
 void listar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
@@ -161,28 +164,25 @@ void listar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, i
     }
 }
 
-void buscar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, int *n_cl, int *n_cm, int *n_cp) {
-    char clienteBusca[30];
+void buscar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha,int *n_cl, int *n_cm, int *n_cp, char *buscaCliente) {
     int buscado = 0;
     int escolha_3;
 
-    
-    switch (escolha){
-    case 5:
-        // se não tiver cliente
-        if (*n_cl == 0){
-            printf("\nNenhum cliente cadastrado.\n");
-            return;
-        }
+    // se não tiver cliente
+    if (*n_cl == 0){
+        printf("\nNenhum cliente cadastrado.\n");
+        return;
+    }      
         // aqui da pra buscar o nome
-        printf("Digite o nome que voce deseja buscar:\n");
-        while (getchar() != '\n');
-        fgets(clienteBusca, sizeof(clienteBusca), stdin);
-        clienteBusca[strcspn(clienteBusca, "\n")] = '\0';
+        printf("Digite o nome que voce deseja buscar:\n"); // interação
+        while (getchar() != '\n'); //Isso vai limpar o buffer
+        fgets(buscaCliente, 30, stdin); // digitar
+        buscaCliente[strcspn(buscaCliente, "\n")] = '\0'; // tira o \n do final
 
         // aqui mostra o nome buscado
         for (int i = 0; i < *n_cl; i++){
-            if (strcmp(cliente[i].nome, clienteBusca) == 0){
+            if (strcmp(cliente[i].nome, buscaCliente) == 0)// compara com os nomes ja digitados
+            {
                 printf("ID: %d | Nome: %s | Telefone: %s | Endereco: %s\n", cliente[i].id, cliente[i].nome, cliente[i].telefone, cliente[i].endereco);
                 buscado = 1;
             }
@@ -190,32 +190,27 @@ void buscar(Cliente *cliente, Camiseta *camiseta, Compra *compra, int escolha, i
         // mano aqui eu to querendo fazer com que ja chame a função direto, alguma ideia?
         if (buscado){
             printf("\nOque deseja fazer?\n1 - Remover\n2 - Atualizar\n3 - Voltar\n");
-            escolha_3 = malloc(TAM*sizeof(int));
-            while (getchar() != '\n');
+            scanf("%d", &escolha_3); // escolha doq fazer
+            while (getchar() != '\n'); // limpa o buffer, se tiver
 
-            /*if (escolha_3 == 1)
+            if (escolha_3 == 1)
             {
-                Remover();
+                remover(cliente, camiseta, compra, escolha, n_cl, n_cm, n_cp, buscaCliente); // chama função remover
             }else if (escolha_3 == 2)
             {
-                Atualizar();
+                atualizar(cliente, camiseta, compra, escolha, n_cl, n_cm, n_cp, buscaCliente); // chama função atualizar
             }else if (escolha_3 == 3)
             {
                 return;
             }else{
                 printf("Digite um valor válido!");
-            }*/
+            }
             
             
             
         }else{
             printf("Cliente nao encontrado\n");
         }
-        break;
-    
-    default:
-        break;
-    }
 }
 
 /*int Mexer(int escolha, char *mexer) {
@@ -232,6 +227,7 @@ int main() {
     //ent, tem q ver esse tanto de varíavel, pq eu acho q deve ficar melhor se diminuir, mas n sei
     int escolha_1, escolha_2, escolha_3, n_cl=0, n_cm=0, n_cp=0;
     int *n1, *n2, *n3;
+    char busca[30];
     n1=&n_cl;
     n2=&n_cm;
     n3=&n_cp;
@@ -262,18 +258,7 @@ int main() {
                     break;*/
                 case 4: listar(cliente, camiseta, compra, escolha_2, n1, n2, n3);
                     break;
-                case 5 : buscar(cliente, camiseta, compra, escolha_2, n1, n2, n3);
-                    {
-                        if (escolha_3 == 1)
-                        {
-                            remover(cliente, camiseta, compra, escolha_2, n1, n2, n3);
-                        }
-                        if (escolha_3 == 2)
-                        {
-                            atualizar(cliente, camiseta, compra, escolha_2, n1, n2, n3);
-                        }
-                        
-                    }
+                case 5 : buscar(cliente, camiseta, compra, escolha_2, n1, n2, n3, busca);
                     break;
                 /*case 6: printf("Voltando\n");
                     break;*/
